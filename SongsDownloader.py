90cde0761bb3e123a4409bbd1fe2efef0abbde28
Rodrigo Shu Liu <70068342+RodrigoLiu1710@users.noreply.github.com>
2020-12-25 03:39:06 +0000

    Add files via upload

---
+++ SongsDownloader.py
@@ -0,0 +1,26 @@
+import spotipy
+import urllib.parse
+import urllib.request
+import re
+from pytube import YouTube
+from spotipy.oauth2 import SpotifyClientCredentials
+
+path = input("Enter the path of your file: ") #C:\Users\---\Desktop\
+uri = input("Enter the Spotify playlist uri: ") #spotify:playlist:4FroAeQwZrJCYYyJroHd9V
+spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(
+    client_id='b714b08a81b3470ab939354358c324c8', client_secret='83a32642aa4646009294ac6430b65dbe'))
+
+songs = spotify.playlist_items(uri)
+tracks = []
+
+for i, playlist in enumerate(songs['items']):
+    # [[Songs0, Artist0], [Songs1, Artist1]]
+    tracks.append([songs['items'][i]['track']['name'], songs['items'][i]['track']['artists'][0]['name']])
+
+for song in tracks:
+    songToSearch = ' '.join([song[0], song[1]])
+    query = urllib.parse.quote(songToSearch)
+    url = "https://www.youtube.com/results?search_query=" + query
+    html = urllib.request.urlopen(url)
+    video_links = re.findall(r'watch\?v=(\S{11})', html.read().decode())
+    YouTube('https://www.youtube.com/watch?v=' + video_links[0]).streams.first().download(path)
